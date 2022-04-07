89e2c686e01d085451536e4c66181d651f741c27
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-07 13:25:04 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  *main -entry point
  *
- * Return: 0 after printing the function
+ *Return: 0 after printing the function
  */
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
