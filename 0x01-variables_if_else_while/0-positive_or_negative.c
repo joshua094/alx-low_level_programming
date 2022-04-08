0e53767d9feffa4e18f04df8d56713a01a39a6e7
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-08 10:17:43 -0700

    printing the value of a random numer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,28 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/**
- 	* main -Entry point
- 	*
- 	* Return: Always 0 (Success)
- 	*/
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
