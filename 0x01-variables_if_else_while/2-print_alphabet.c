97dcd6281179f28ae021aa43c2e9097ada92d5fa
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-08 10:53:24 -0700

    Printing alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char alpha;
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 	{
 		putchar(alpha);
 	}
