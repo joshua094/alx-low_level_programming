5ebfb55f94de92471b7d477ccc656cd4b3738b7a
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-08 12:46:11 -0700

    printing hexadecimal characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar (m);
+	}
+	
+	char j;
+
+	for (j = 97; j < 102; j++)
+	{
+		putchar (j);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
