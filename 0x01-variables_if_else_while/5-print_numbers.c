5e3acee432cb44159fdf3c7dc657e386b7ba05b9
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-08 11:35:45 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		printf("%d\n", m)
+	}
+
+	return 0;
+}
