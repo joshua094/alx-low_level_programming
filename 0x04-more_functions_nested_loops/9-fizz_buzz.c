0008c08262cfa3c2d9554ffb9fc1ad202c071bb7
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-14 15:42:24 -0700

    fizz buzz fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,12 +20,12 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3) == 0 && (i % 5) ==);
+		else if ((i % 3) == 0 && (i % 5) ==0);
 		{
 			printf("FizzBuzz ");
 		}
 		else
-			print("i ");
+			print("%d ", i);
 	}
 	printf("\n");
 
