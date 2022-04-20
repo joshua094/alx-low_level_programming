7934bfb1d290f1d1c39d95cd44e8d4c98b373238
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-20 16:06:21 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse order
+ * @s: pointer to char
+ * Return: Always zero
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+	for (x--; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
