d4928a8c446216a0f17c13ec97cca06e0849b850
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-20 15:59:22 -0700

    resettting with a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer and resets it t0 98
+ * @n: pointer to an int
+ * return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
