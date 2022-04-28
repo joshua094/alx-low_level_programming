3ad78a2a54370bce8847b053e8a3897e1f5c1c84
Emmanuel Amoo <amooemma10@gmail.com>
2022-04-28 07:20:14 -0700

    reversing a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,24 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * _print_rev_recursion - print a string in reverse
  * @s: string
  * Return: Always (0)
  */
-
 void _print_rev_recursion(char *s)
 {
-	static int i, len, temp;
-	len = strlen(s);
 	if (*s == '\0')
 		return;
-	else
-	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
-		i++;
-		_print_rev_recursion(s);
-		_putchar(*s);
-	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
