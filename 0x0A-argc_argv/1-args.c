7b93dcfd0a947d2d587440c235b186b492c1b0a0
Emmanuel Amoo <amooemma10@gmail.com>
2022-05-02 18:03:06 -0700

    argc-1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: size  of argv
+ * @argv: array of size argc
+ * Return: Always 0 Success
+ */
+int main(int argc, char* argv[])
+{
+	printf("%d\n", (argc-1));
+
+	return(0);
+}
