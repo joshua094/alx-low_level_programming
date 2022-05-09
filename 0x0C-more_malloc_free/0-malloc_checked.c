804ac0083f27cc98630df9bf2c16beda23338551
Emmanuel Amoo <amooemma10@gmail.com>
2022-05-09 13:49:24 -0700

    checking malloc fate

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * main - check if code correctly allocates malloc
- *  @b: number of bits to use
- * Return: pointer to the address or 98 otherwise
+ * malloc_checked - checks whether malloc succeeded
+ * @b: number of bits to use
+ * Return: pointer to address or 98 otherwise
  */
-
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
-    if (ptr == NULL)
-        exit (98);
-
-return (ptr);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
